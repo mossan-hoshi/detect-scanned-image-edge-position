edc23c7d5d92b827040186a3c1279011d9060127
mossan-hoshi <sackn5@gmail.com>
2021-10-03 22:00:28 +0900

    debug test

--- tests/test_detect_scanned_image_edge_position.py
+++ tests/test_detect_scanned_image_edge_position.py
@@ -2,18 +2,26 @@ from src.detect_scanned_image_edge_position import detect_scanned_image_edge_pos
 from pathlib import Path
 import cv2
 import numpy as np
+import pickle
+import argparse
 
 
-def test_idetect_scanned_image_edge_position():
+def test_detect_scanned_image_edge_position():
     # set arguments
     base_dir_path = Path.cwd()
     in_file_path = base_dir_path / "data/sample.jpg"
-    exp_file_path = base_dir_path / "tests/exp/image_process_sample.png"
+    exp_file_path = base_dir_path / "tests/exp/detect_scanned_image_edge_position.pkl"
+    args = {"background_is_black": False}
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--background_is_black", type=bool)
+    args = parser.parse_args(["--background_is_black", False])
 
-    # read images
+    # run test
     in_image = cv2.imread(str(in_file_path))
-    exp_image = cv2.imread(str(exp_file_path))
+    result_image, result_data = detect_scanned_image_edge_position(in_image, args)
 
-    debug_image, result_data = detect_scanned_image_edge_position(in_image, None)
+    # check result
+    with open(exp_file_path, "rb") as f:
+        exp_data = pickle.load(f)
 
-    assert np.array_equal(debug_image, exp_image)
+    assert np.array_equal(result_data, exp_data)
